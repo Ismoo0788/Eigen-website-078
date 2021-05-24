c11d940346d573e77a932a8dd4401f5a1761017f
Ismoo0788 <72944162+Ismoo0788@users.noreply.github.com>
2021-05-24 22:34:09 +0200

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,2 @@
+console.log(hallo + ismoo);
+opdracht_1
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VerFox</title>
+    <link rel="stylesheet" href="index1.html">
+</head>
+<body>
+    
+<?php
+$tekst = 'kes jwz';
+echo $tekst;
+?>
+
+<?php
+$tekst = 'ismail dordrecht';
+echo $tekst;
+?>
+
+<?php
+$aFruit = array("appel", "peer", "banaan", "kiwi");
+?>
+
+
+</body>
+</html>
